dc4191519d010a56f96d85ff1a6b1c85fc5bb550
Amit G <amitgururaj2003@gmail.com>
2024-06-07 22:39:04 +0530

    Update todo.component.ts

---
+++ todo.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+export class TodoComponent {
+  newTodo: string = '';
+  todos: string[] = [];
+
+  addTodo() {
+    if (this.newTodo.trim() !== '') {
+      this.todos.push(this.newTodo);
+      this.newTodo = '';
+    }
+  }
+
+  removeTodo(index: number) {
+    this.todos.splice(index, 1);
+  }
+}
